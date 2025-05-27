938a47caf8622ce5b7ef7cd2a09347b0a1f3f9f9
sarangan777 <149357922+sarangan777@users.noreply.github.com>
2025-05-27 14:42:33 +0530

    Updated package-lock.json

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/messaging';
+import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
